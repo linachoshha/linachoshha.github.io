c516fa427b5efecd61b4ba48b27df16fce1f1e11
linachoshha <linachosha@hotmail.com>
2025-12-15 12:11:55 +0000

    Deploying to gh-pages from @ linachoshha/linachoshha.github.io@06813aceca264bd7e096f467003bff7a0262bfd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/books/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-x-post",
+        
+          title: "X post",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/twitter/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
