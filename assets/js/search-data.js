a3f57736c78fecb0f7fd48be6fabfcd528113c96
linachoshha <linachosha@hotmail.com>
2025-12-29 11:54:09 +0000

    Deploying to gh-pages from @ linachoshha/linachoshha.github.io@2f0e64efbef7fa2e03481bf4fbe057c87f18bb1a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "nav-cv",
           title: "CV",
